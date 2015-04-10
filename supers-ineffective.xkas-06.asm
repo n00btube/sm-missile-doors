73319087d6199c6ea9d1538d898f1c0be00ed38f
Adam <snescoder@gmail.com>
2015-04-09 20:18:35 -0400

    Note that there's a hex tweak.  Rework comments.

--- supers-ineffective.xkas-06.asm
+++ supers-ineffective.xkas-06.asm
@@ -2,11 +2,18 @@
 ; MIT license.
 ; There _could_ be newer versions available on GitHub.
 
+; Derp.  JAM found a hex tweak for this very thing.  It just NOPs out the
+; super missile CMP/BEQ right there at $84:BD58.
+;
+; The only reasons to use THIS are:
+; 1. if you like ASM as much as I do
+; 2. if wasting cycles on five NOPs per projectile hit bugs you
+
 lorom
 
 ; red door hit detection: rewrite in place, no free space used
 org $84BD58
-	CMP #$0100  ; regular missile?
-	BEQ $05     ; $BD62, after super missile check we're about to nuke
-	JMP $BD74   ; it's never a super missile
+	CMP #$0100  ; check regular missile instead of super
+	BEQ $05     ; $BD62, regular missile code
+	JMP $BD74   ; non-reaction code
 
